2d95a27d0807ce1100821997c657be13f4b5c7c3
Alexis <ayrenay@users.noreply.github.com>
2017-05-23 08:58:35 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//var sample_data = [5, 10, 15, 20, 25];
+/*var top_10 = [
+    {'movie': 'The Avengers', 'release_year': 2012, 'infl_adj_sales': 677307543}
+    , {'movie': 'The Dark Knight', 'release_year': 2008, 'infl_adj_sales': 642419549}
+    , {'movie': 'Avengers: Age of Ultron', 'release_year': 2015, 'infl_adj_sales': 470984663}
+    , {'movie': 'The Dark Knight Rises', 'release_year': 2012, 'infl_adj_sales': 486985321}
+    , {'movie': 'Iron Man 3', 'release_year': 2013, 'infl_adj_sales': 435151675}
+    , {'movie': 'Captain America: Civil War', 'release_year': 2016, 'infl_adj_sales': 408084349}
+    , {'movie': 'Spider-Man', 'release_year': 2002, 'infl_adj_sales': 601043053}
+    , {'movie': 'Spider-Man 2', 'release_year': 2004, 'infl_adj_sales': 520287726}
+    , {'movie': 'Deadpool', 'release_year': 2016, 'infl_adj_sales': 363070709}
+    , {'movie': 'Spider-Man 3', 'release_year': 2007, 'infl_adj_sales': 423108591}
+];*/
